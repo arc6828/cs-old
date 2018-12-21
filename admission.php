2f652db83dabb9ec762ec6b3dec5394e34379870
arc6828 <chavalit.kow@gmail.com>
2018-12-21 16:35:20 +0700

    Admission page
    Office

---
+++ admission.php
@@ -0,0 +1,204 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <?php include 'ahead.php';  ?>
+</head>
+<body>
+
+
+<div class="container-fluid">
+  <?php include 'anavbar.php';  ?>
+
+  <!-- start content -->
+  <div class="container">
+    <style>
+      
+      p{
+        font-size: 20px;
+      }
+    </style>
+    <br />
+    <h2>&starf;&nbsp;รับสมัครนักศึกษา (ปริญญาตรี จันทร์-ศุกร์)</h2>
+    <hr>
+
+    
+
+    <p>
+      หลักสูตรวิทยาการคอมพิวเตอร์รับสมัครนักศึกษา TCAS 62 (ปริญญาตรี จันทร์-ศุกร์) ตั้งแต่วันนี้เป็นต้นไป  สนใจดูรายละเอียดและกรอกใบสมัครได้ที่นี่ สอบถามข้อมูลการรับสมัครเพิ่มเติม โทร. 083-138-0981 
+    </p>
+
+    <br />
+
+    <div class="text-center">
+      <img src="images/slide/2.jpg" width="100%" style="max-width: 768px;">
+    </div>
+
+
+    <br />
+
+    <h3>ตารางการรับสมัครนักศึกษา TCAS 62 (ปริญญาตรี จันทร์-ศุกร์)</h3>
+
+    <div id="gallery" class="">
+      <section>
+        <figure>
+        <table>
+        <tbody><tr><td bgcolor="#ABEBC6">
+        <p>
+          <font color="" size="5"><b></b></font>
+          <img src="../images/demo/new_icon/newlight.gif">
+        </p>
+              <table class="table">
+                  <thead>
+                    <tr><th width="30%">กิจกรรม</th>
+                    <th width="">รอบ1 portfolio</th>
+                    <th width="">รอบ2 โควตา</th>
+                    <th width="">รอบ3 รับตรงร่วมกัน</th>
+                    <!--<th width="">รอบ4 Admissions</th>
+                    <th width="">รอบ5 รับตรงอิสระ</th>-->
+                  </tr></thead>              
+                                <tbody>
+                    <tr><td bgcolor="#76EEC6">รับสมัคร</td>
+                    <td bgcolor="#C1FFC1" align="">1-15 ธ.ค. 61<br>http://ent.vru.ac.th</td>
+                    <td bgcolor="#FFFACD" align="">4 ก.พ.-23 มี.ค.62
+<br>http://ent.vru.ac.th</td>
+                    <td bgcolor="#F0FFF0" align="">17-29 เม.ย.62<br>website ทปอ.</td>
+                    <!--<td bgcolor="#C1FFC1"><?php// echo $record["configdate_04"]; ?></td>
+                    <td bgcolor="#C1FFC1"><?php// echo $record["configdate_05"]; ?></td>-->
+                  </tr></tbody>  
+                                <tbody>
+                    <tr><td bgcolor="#76EEC6">ชำระเงินค่าสมัคร</td>
+                    <td bgcolor="#C1FFC1" align="">1-16 ธ.ค. 61<br>เคาน์เตอร์เซอร์วิส 7-11 / งานการเงินมหาวิทยาลัย</td>
+                    <td bgcolor="#FFFACD" align="">4 ก.พ.-24 มี.ค.62<br>เคาน์เตอร์เซอร์วิส 7-11 / งานการเงินมหาวิทยาลัย</td>
+                    <td bgcolor="#F0FFF0" align="">17-29 เม.ย. 62<br></td>
+                    <!--<td bgcolor="#C1FFC1"><?php// echo $record["configdate_04"]; ?></td>
+                    <td bgcolor="#C1FFC1"><?php// echo $record["configdate_05"]; ?></td>-->
+                  </tr></tbody>  
+                                <tbody>
+                    <tr><td bgcolor="#76EEC6">ส่งหลักฐานประกอบการรับสมัคร</td>
+                    <td bgcolor="#C1FFC1" align="">1-15 ธ.ค. 61<br>http://ent.vru.ac.th</td>
+                    <td bgcolor="#FFFACD" align="">4 ก.พ.-23 มี.ค.62<br>http://ent.vru.ac.th</td>
+                    <td bgcolor="#F0FFF0" align="">ในวันสอบสัมภาษณ์<br></td>
+                    <!--<td bgcolor="#C1FFC1"><?php// echo $record["configdate_04"]; ?></td>
+                    <td bgcolor="#C1FFC1"><?php// echo $record["configdate_05"]; ?></td>-->
+                  </tr></tbody>  
+                                <tbody>
+                    <tr><td bgcolor="#76EEC6">ประกาศรายชื่อผู้มีสิทธิ์สอบสัมภาษณ์และห้องสอบสัมภาษณ์</td>
+                    <td bgcolor="#C1FFC1" align="">8 ม.ค. 62<br>http://ent.vru.ac.th</td>
+                    <td bgcolor="#FFFACD" align="">11 เม.ย. 62<br>http://ent.vru.ac.th</td>
+                    <td bgcolor="#F0FFF0" align="">9 พ.ค. 62<br>http://ent.vru.ac.th</td>
+                    <!--<td bgcolor="#C1FFC1"><?php// echo $record["configdate_04"]; ?></td>
+                    <td bgcolor="#C1FFC1"><?php// echo $record["configdate_05"]; ?></td>-->
+                  </tr></tbody>  
+                                <tbody>
+                    <tr><td bgcolor="#76EEC6">สอบสัมภาษณ์</td>
+                    <td bgcolor="#C1FFC1" align="">16 ม.ค. 62<br></td>
+                    <td bgcolor="#FFFACD" align="">19 เม.ย. 62<br></td>
+                    <td bgcolor="#F0FFF0" align="">14 พ.ค. 62<br></td>
+                    <!--<td bgcolor="#C1FFC1"><?php// echo $record["configdate_04"]; ?></td>
+                    <td bgcolor="#C1FFC1"><?php// echo $record["configdate_05"]; ?></td>-->
+                  </tr></tbody>  
+                                <tbody>
+                    <tr><td bgcolor="#76EEC6">ประกาศรายชื่อผู้ผ่านการสอบสัมภาษณ์</td>
+                    <td bgcolor="#C1FFC1" align="">22 ม.ค. 62<br>http://ent.vru.ac.th</td>
+                    <td bgcolor="#FFFACD" align="">22 เม.ย. 62<br>http://ent.vru.ac.th</td>
+                    <td bgcolor="#F0FFF0" align="">17 พ.ค. 62<br>http://ent.vru.ac.th</td>
+                    <!--<td bgcolor="#C1FFC1"><?php// echo $record["configdate_04"]; ?></td>
+                    <td bgcolor="#C1FFC1"><?php// echo $record["configdate_05"]; ?></td>-->
+                  </tr></tbody>  
+                                <tbody>
+                    <tr><td bgcolor="#76EEC6">ส่งรายชื่อเข้าระบบ TCAS</td>
+                    <td bgcolor="#C1FFC1" align="">26 ม.ค. 62<br></td>
+                    <td bgcolor="#FFFACD" align="">22 เม.ย. 62<br></td>
+                    <td bgcolor="#F0FFF0" align="">15 พ.ค. 62<br></td>
+                    <!--<td bgcolor="#C1FFC1"><?php// echo $record["configdate_04"]; ?></td>
+                    <td bgcolor="#C1FFC1"><?php// echo $record["configdate_05"]; ?></td>-->
+                  </tr></tbody>  
+                                <tbody>
+                    <tr><td bgcolor="#76EEC6">ยืนยันสิทธิ์ Clearing House 
+เข้าเรียน
+</td>
+                    <td bgcolor="#C1FFC1" align="">30-31 ม.ค. 62<br></td>
+                    <td bgcolor="#FFFACD" align="">24-25 เม.ย. 62<br></td>
+                    <td bgcolor="#F0FFF0" align="">อัตโนมัติ<br></td>
+                    <!--<td bgcolor="#C1FFC1"><?php// echo $record["configdate_04"]; ?></td>
+                    <td bgcolor="#C1FFC1"><?php// echo $record["configdate_05"]; ?></td>-->
+                  </tr></tbody>  
+                                <tbody>
+                    <tr><td bgcolor="#76EEC6">ประกาศรายชื่อผู้มีสิทธิ์เข้าศึกษา</td>
+                    <td bgcolor="#C1FFC1" align="">5 ก.พ. 62<br>http://ent.vru.ac.th</td>
+                    <td bgcolor="#FFFACD" align="">29 เม.ย. 62<br>http://ent.vru.ac.th</td>
+                    <td bgcolor="#F0FFF0" align="">17 พ.ค. 62<br>http://ent.vru.ac.th</td>
+                    <!--<td bgcolor="#C1FFC1"><?php// echo $record["configdate_04"]; ?></td>
+                    <td bgcolor="#C1FFC1"><?php// echo $record["configdate_05"]; ?></td>-->
+                  </tr></tbody>  
+                                <tbody>
+                    <tr><td bgcolor="#76EEC6">พิมพ์ใบแจ้งชำระเงินค่าธรรมเนียมแรกเข้า</td>
+                    <td bgcolor="#C1FFC1" align="">11-17 ก.พ. 62<br>http://ent.vru.ac.th</td>
+                    <td bgcolor="#FFFACD" align="">6-12 พ.ค. 62 <br>http://ent.vru.ac.th</td>
+                    <td bgcolor="#F0FFF0" align="">20-26 พ.ค.62<br>http://ent.vru.ac.th</td>
+                    <!--<td bgcolor="#C1FFC1"><?php// echo $record["configdate_04"]; ?></td>
+                    <td bgcolor="#C1FFC1"><?php// echo $record["configdate_05"]; ?></td>-->
+                  </tr></tbody>  
+                                <tbody>
+                    <tr><td bgcolor="#76EEC6">ชำระเงินค่าธรรมเนียมแรกเข้า</td>
+                    <td bgcolor="#C1FFC1" align="">11-17 ก.พ. 62<br>เคาน์เตอร์เซอร์วิส 7-11 / งานการเงินมหาวิทยาลัย</td>
+                    <td bgcolor="#FFFACD" align="">6-12 พ.ค. 62<br>เคาน์เตอร์เซอร์วิส 7-11 / งานการเงินมหาวิทยาลัย</td>
+                    <td bgcolor="#F0FFF0" align="">20-26 พ.ค.62<br>เคาน์เตอร์เซอร์วิส 7-11 / งานการเงินมหาวิทยาลัย</td>
+                    <!--<td bgcolor="#C1FFC1"><?php// echo $record["configdate_04"]; ?></td>
+                    <td bgcolor="#C1FFC1"><?php// echo $record["configdate_05"]; ?></td>-->
+                  </tr></tbody>  
+                              
+              </table>
+              <table>
+                <tbody>
+                                </tbody><tbody>
+                    <tr><td width="30%" bgcolor="#76EEC6">พิมพ์ใบแจ้งชำระเงินค่าธรรมเนียมการศึกษา</td>
+                    <td bgcolor="#CCFF99" align="">วันที่ 22-31 พฤษภาคม 2562  http://ent.vru.ac.th</td>
+                  </tr></tbody>
+                                <tbody>
+                    <tr><td width="30%" bgcolor="#76EEC6">ชำระเงินค่าธรรมเนียมการศึกษา</td>
+                    <td bgcolor="#CCFF99" align="">วันที่ 22-31 พฤษภาคม 2562 เคาน์เตอร์เซอร์วิส 7-11 / งานการเงินมหาวิทยาลัย</td>
+                  </tr></tbody>
+                                <tbody>
+                    <tr><td width="30%" bgcolor="#76EEC6">ปฐมนิเทศนักศึกษาใหม่</td>
+                    <td bgcolor="#CCFF99" align="">วันที่ 3 มิถุนายน 2562 </td>
+                  </tr></tbody>
+                                <tbody>
+                    <tr><td width="30%" bgcolor="#76EEC6">ทดสอบภาษาอังกฤษ</td>
+                    <td bgcolor="#CCFF99" align="">วันที่ 4-6 มิถุนายน 2562 </td>
+                  </tr></tbody>
+                                <tbody>
+                    <tr><td width="30%" bgcolor="#76EEC6">กิจกรรมของกองพัฒนานักศึกษา/คณะ</td>
+                    <td bgcolor="#CCFF99" align="">วันที่ 7-9 มิถุนายน 2562 </td>
+                  </tr></tbody>
+                                <tbody>
+                    <tr><td width="30%" bgcolor="#76EEC6">เปิดภาคการศึกษาที่1/2562</td>
+                    <td bgcolor="#CCFF99" align="">วันที่ 10 มิถุนายน 2562 </td>
+                  </tr></tbody>
+                                
+                
+              </table>
+              <font color="" size=""><b>รอบ4 Admissions</b></font>&gt;&gt; มหาวิทยาลัยราชภัฏวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์ไม่เข้าร่วมรับสมัครนักศึกษาในรอบ 4 Admissions<br>
+              <font color="" size=""><b>รอบ5 รับตรงอิสระ</b></font>&gt;&gt; มหาวิทยาลัยราชภัฏวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์จะแจ้งกำหนดการให้ทราบในภายหลัง
+            <hr> 
+            <!--##############################################################################################################-->
+        </td>
+        </tr></tbody></table>            
+            <!--##############################################################################################################-->                  
+        </figure>
+      </section>
+    </div>
+    
+    <div class="text-center">
+
+      <a class="btn btn-success" style="color:white;">กรอกใบสมัคร</a>   
+
+    </div>
+  </div>
+  <!-- end content -->
+
+  <br/>
+  <?php include 'afooter.php';  ?>     
+  </div>   
+</body>
+</html>
