5b3e7f7aa66475031088c9c4e1cdc0761ad2976a
arc6828 <chavalit.kow@gmail.com>
2019-06-22 14:59:27 +0700

    Test

--- smart/routes/web.php
+++ smart/routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/publication/test', function(){
+  return view('publication/test');
+});
 Route::resource('/publication', 'PublicationController');
 
 Auth::routes();
